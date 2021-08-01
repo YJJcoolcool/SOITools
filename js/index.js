f9c875ade4cdf46570a8b04488ce1a5d86f27f17
Yam Jun Jie <yjjcoolcool@gmail.com>
2021-08-01 17:36:13 +0800

    Revert "Did a lot of stuff"
    This reverts commit 5d74e2825a1238160dfde1ca5b65bb95cc19e8b2.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 function checkvalidip(id){
-    const ip1 = document.getElementById('ip1').value;
     const classtype = document.getElementById('classtype');
-
     if (document.getElementById(id).value>255){
         document.getElementById(id).value=255;
     }
@@ -9,16 +7,31 @@ function checkvalidip(id){
         document.getElementById(id.substr(0,2)+(parseInt(id.substr(2,3))+1)).focus();
         document.getElementById(id.substr(0,2)+(parseInt(id.substr(2,3))+1)).select();
     }
+    checkipclass();
+}
+
+function checkipclass(){
+    const ip1 = document.getElementById('ip1').value;
     if (ip1<127){
         classtype.innerText="Class A Address";
+        document.getElementById('slider').value=8;
+        maskbits(8);
     } else if (ip1==127){
         classtype.innerText="Loopback Address";
-    } else if (ip1<191){
+        document.getElementById('slider').value=16;
+        maskbits(16);
+    } else if (ip1<=191){
         classtype.innerText="Class B Address";
-    } else if (ip1<223){
+        document.getElementById('slider').value=16;
+        maskbits(16);
+    } else if (ip1<=223){
         classtype.innerText="Class C Address";
+        document.getElementById('slider').value=24;
+        maskbits(24);
     } else {
         classtype.innerText="Invalid IP Address";
+        document.getElementById('slider').value=24;
+        maskbits(24);
     }
 }
 
@@ -54,7 +67,7 @@ function maskbits(bits){
             maxsubnets.innerText="Max Subnets: "+(2**(bits-24));
             hostbits.innerText="Host Bits: "+(8-(bits-24));
             ((2**(8-(bits-24))-2)<0) ? maxhosts.innerText="Max Hosts: 0" : maxhosts.innerText="Max Hosts: "+(2**(8-(bits-24))-2);
-            if (bits>31) subnetmode.innerText="Mask too long, no hosts.";
+            if (bits>30) subnetmode.innerText="Mask too long, no hosts.";
             else if (bits>24) subnetmode.innerText="Warning! Subnet - All 0's";
             pass=true;
         }
@@ -69,5 +82,27 @@ function maskbits(bits){
     var final = "255.".repeat(groups)+groupbits[remainder];
     (remainder==0) ? final+="0.".repeat(4-groups) : final+="0.".repeat(3-groups);
     document.getElementById('wildcard').innerText=final.substr(0,final.length-1);
+    document.getElementById('subnetmask').innerText="Subnet Mask: "+final.substr(0,final.length-1);
+    updatehostrange(final.substr(0,final.length-1));
+}
 
+function updatehostrange(subnetmask){
+    const ip1 = document.getElementById('ip1').value;
+    const ip2 = document.getElementById('ip2').value;
+    const ip3 = document.getElementById('ip3').value;
+    const ip4 = document.getElementById('ip4').value;
+    var final = "";
+    if (subnetmask.startsWith("255.")) {
+        final+=ip1+".";
+        if (subnetmask.startsWith("255.255.")) {
+            final+=ip2+".";
+            if (subnetmask.startsWith("255.255.255.")) {
+                final+=ip3+".";
+                if (subnetmask.startsWith("255.255.255.255")) {
+                    final+=ip4;
+                }
+            }
+        }
+    }
+    document.getElementById('hostrange').value=final;
 }
